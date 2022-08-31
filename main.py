53dbde249767fefe9440fa0d5091c2bffcade3b5
Abraham <abraham1255@gmail.com>
2022-08-31 14:40:57 -0500

    Revert "Version con Páginado"
    This reverts commit ab42c33880dc04511f9b4ae0a07d95a4fe5b19c0.

--- main.py
+++ main.py
@@ -21,93 +21,57 @@ enviroment = config['development']
 app = create_app(enviroment)
 jwt = JWTManager(app)
     
-# metodo login para obtener Access token
-@app.route('/api/v1/login', methods=['POST'])
+# metodo login para obtener token
+@app.route("/api/v1/login", methods=["POST"])
 def login():
-    username = request.json.get('username', None)
-    password = request.json.get('password', None)
-    if username != 'test' or password != 'test':
-        return jsonify({'msg': 'Credenciales Incorrectas'}), 401
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
 
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token)
 
 # metodo para obtener colonias por CP requiere enviar token en header
-@app.route('/api/v1/colonias/<cp>/<int:page>/<int:maxItems>', methods=['GET'])
+@app.route('/api/v1/colonias/<cp>', methods=['GET'])
 @jwt_required()
-def get_colonias(cp,page,maxItems):
-    search = '%{}%'.format(cp)
-    colonias = CodigoPostal.query.filter(CodigoPostal.codigo_postal.ilike(search)).paginate(page,maxItems)
+def get_colonias(cp):
+    search = "%{}%".format(cp)
+    colonias = CodigoPostal.query.filter(CodigoPostal.codigo_postal.ilike(search)).all()
     if not colonias:
         return jsonify(colonias),204
-    results = {
-        "results": [{"colonia": m.colonia, "cp":m.codigo_postal,"asentamiento":m.asentamiento,"ciudad":m.ciudad,"municipio":m.municipio} for m in colonias.items],
-        "pagination": {
-            "count": colonias.total,
-            "page": page,
-            "per_page": maxItems,
-            "pages": colonias.pages,
-        },
-    }
-    return jsonify(results)
+    return jsonify(colonias)
 
 
-@app.route('/api/v1/coloniasByName/<nombre>/<int:page>/<int:maxItems>', methods=['GET'])
+@app.route('/api/v1/coloniasByName/<nombre>', methods=['GET'])
 # metodo para obtener colonias por nombre requiere enviar token en header
 @jwt_required()
-def get_coloniasByName(nombre,page,maxItems):
-    search = '%{}%'.format(nombre)
-    colonias = CodigoPostal.query.filter(CodigoPostal.colonia.ilike(search)).paginate(page,maxItems)
+def get_coloniasByName(nombre):
+    search = "%{}%".format(nombre)
+    colonias = CodigoPostal.query.filter(CodigoPostal.colonia.ilike(search)).all()
     if not colonias:
         return jsonify(colonias),204
-    results = {
-        "results": [colonias.items],
-        "pagination": {
-            "count": colonias.total,
-            "page": page,
-            "per_page": maxItems,
-            "pages": colonias.pages,
-        },
-    }
-    return jsonify(results)
+    return jsonify(colonias)
 
-@app.route('/api/v1/municipiosByName/<nombre>/<int:page>/<int:maxItems>', methods=['GET'])
+@app.route('/api/v1/municipiosByName/<nombre>', methods=['GET'])
 # metodo para obtener municipios por nombre requiere enviar token en header
 @jwt_required()
-def get_municipiosByName(nombre,page,maxItems):
-    search = '%{}%'.format(nombre)
-    municipios = Municipio.query.filter(Municipio.nombre.ilike(search)).paginate(page,maxItems)
+def get_municipiosByName(nombre):
+    search = "%{}%".format(nombre)
+    municipios = Municipio.query.filter(Municipio.nombre.ilike(search)).all()
     if not municipios:
         return jsonify(municipios),204
-    results = {
-        "results": [municipios.items],
-        "pagination": {
-            "count": municipios.total,
-            "page": page,
-            "per_page": maxItems,
-            "pages": municipios.pages,
-        },
-    }
-    return jsonify(results)
+    return jsonify(municipios)
 
-@app.route('/api/v1/estadosByName/<nombre>/<int:page>/<int:maxItems>', methods=['GET'])
+@app.route('/api/v1/estadosByName/<nombre>', methods=['GET'])
 # metodo para obtener estados por nombre requiere enviar token en header
 @jwt_required()
-def get_estadosByName(nombre,page,maxItems):
-    search = '%{}%'.format(nombre)
-    estados = Estado.query.filter(Estado.nombre.ilike(search)).paginate(page,maxItems)
+def get_estadosByName(nombre):
+    search = "%{}%".format(nombre)
+    estados = Estado.query.filter(Estado.nombre.ilike(search)).all()
     if not estados:
         return jsonify(estados),204
-    results = {
-        "results": [estados.items],
-        "pagination": {
-            "count": estados.total,
-            "page": page,
-            "per_page": maxItems,
-            "pages": estados.pages,
-        },
-    }
-    return jsonify(results)
+    return jsonify(estados)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
